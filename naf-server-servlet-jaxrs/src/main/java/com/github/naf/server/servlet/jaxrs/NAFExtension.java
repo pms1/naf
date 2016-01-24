4e11ca074511555690781be14f00259cbb6c3f69
Mirko Streckenbach <pms1@users.noreply.github.com>
2016-01-24 07:08:21 +0100

    cleanup

--- naf-server-servlet-jaxrs/src/main/java/com/github/naf/server/servlet/jaxrs/NAFExtension.java
+++ naf-server-servlet-jaxrs/src/main/java/com/github/naf/server/servlet/jaxrs/NAFExtension.java
@@ -9,27 +9,26 @@ import javax.ws.rs.ext.Provider;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.model.Resource;
+import org.glassfish.jersey.servlet.ServletContainer;
 
 import com.github.naf.server.servlet.ServletInitializationEvent;
 import com.github.naf.spi.Extension;
 
 public class NAFExtension implements Extension {
 	void init(@Observes ServletInitializationEvent event, BeanManager bm) {
-		ServletHolder jerseyServlet = event.getContext().addServlet(org.glassfish.jersey.servlet.ServletContainer.class,
-				"/*");
+		ServletHolder jerseyServlet = event.getContext().addServlet(ServletContainer.class, "/*");
 		jerseyServlet.setInitOrder(0);
 
 		// Tells the Jersey servlet to load all classes annotated with
-		// "@Path"
+		// "@Path" or "@Provider".
 		jerseyServlet.setInitParameter(ServerProperties.PROVIDER_CLASSNAMES,
 				bm.getBeans(Object.class).stream()//
 						.map(b -> b.getBeanClass()) //
 						.filter(c -> Resource.getPath(c) != null || c.isAnnotationPresent(Provider.class)) //
 						.map(c -> c.getCanonicalName()) //
 						.collect(Collectors.joining(" ")));
-		System.err.println("PARAMS " + jerseyServlet.getInitParameter(ServerProperties.PROVIDER_CLASSNAMES));
 
-		// suppress Content-Length for streaming output
+		// suppress "Content-Length" HTTP header to allow streaming output
 		jerseyServlet.setInitParameter(ServerProperties.OUTBOUND_CONTENT_LENGTH_BUFFER, "0");
 	}
 }
